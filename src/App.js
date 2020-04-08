beab494a545ea028885f7072472ee50e75b409b6
unknown <aindriu80@gmail.com>
2020-04-08 11:44:10 +0100

    Basic Routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import './App.css';
+import { Route, Router, Switch, BrowserRouter } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage-component.jsx';
+
+const HatsPage = () => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/hats" component={HatsPage} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
