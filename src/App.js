b81aedffa75656362c059ba1521537f7ab5b79dc
unknown <aindriu80@gmail.com>
2020-04-23 16:49:49 +0100

    Sign In Page fix

--- src/App.js
+++ src/App.js
@@ -63,7 +63,7 @@ class App extends React.Component {
   }
 }
 
-const mapStateToProps = createStructuredSelector => ({
+const mapStateToProps = createStructuredSelector({
   currentUser: selectCurrentUser
 });
 
