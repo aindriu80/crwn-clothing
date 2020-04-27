1d51243cc26cfc95d4c188c701c9c3b0914899ab
unknown <aindriu80@gmail.com>
2020-04-27 15:55:28 +0100

    Nested Routing in the Shop Page

---
+++ src/pages/collection/collection.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+// import CollectionItem from '../../components/Collection/CollectionItem';
+
+import './collection.styles.scss';
+
+const CollectionPage = ({ match }) => (
+  <div className="collection">
+    <h2>Collection Page</h2>
+  </div>
+);
+
+export default CollectionPage;
