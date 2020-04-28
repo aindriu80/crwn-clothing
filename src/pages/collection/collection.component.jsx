c1e2538085f16034a4decba31bb4bd3496921628
unknown <aindriu80@gmail.com>
2020-04-28 09:58:42 +0100

    Collection Routing and Selector

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
+
+import { connect } from 'react-redux';
+
 // import CollectionItem from '../../components/Collection/CollectionItem';
+
+import { selectCollection } from '../../redux/shop/shop.selectors';
 
 import './collection.styles.scss';
 
-const CollectionPage = ({ match }) => (
+const CollectionPage = ({ collection }) => (
   <div className="collection">
     <h2>Collection Page</h2>
   </div>
 );
 
-export default CollectionPage;
+const mapStateToProps = (state, ownProps) => ({
+  collection: selectCollection(ownProps.match.params.collectionId)(state)
+});
+
+export default connect(mapStateToProps)(CollectionPage);
