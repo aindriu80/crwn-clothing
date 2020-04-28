cdb55675b978d2a42c63931ef7f64e07716dc7be
unknown <aindriu80@gmail.com>
2020-04-28 11:01:32 +0100

    Data Normalization & Collections Page

--- src/redux/shop/shop.data.jsx
+++ src/redux/shop/shop.data.jsx
@@ -1,5 +1,5 @@
-const SHOP_DATA = [
-  {
+const SHOP_DATA = {
+  hats: {
     id: 1,
     title: 'Hats',
     routeName: 'hats',
@@ -55,12 +55,12 @@ const SHOP_DATA = [
       {
         id: 9,
         name: 'Blue Snapback',
-        imageUrl: '../images/shop-img/hats/lue-snapback.png',
+        imageUrl: '../images/shop-img/hats/blue-snapback.png',
         price: 16
       }
     ]
   },
-  {
+  sneakers: {
     id: 2,
     title: 'Sneakers',
     routeName: 'sneakers',
@@ -115,7 +115,7 @@ const SHOP_DATA = [
       }
     ]
   },
-  {
+  jackets: {
     id: 3,
     title: 'Jackets',
     routeName: 'jackets',
@@ -152,7 +152,7 @@ const SHOP_DATA = [
       }
     ]
   },
-  {
+  womens: {
     id: 4,
     title: 'Womens',
     routeName: 'womens',
@@ -201,7 +201,7 @@ const SHOP_DATA = [
       }
     ]
   },
-  {
+  mens: {
     id: 5,
     title: 'Mens',
     routeName: 'mens',
@@ -244,6 +244,6 @@ const SHOP_DATA = [
       }
     ]
   }
-];
+};
 
 export default SHOP_DATA;
