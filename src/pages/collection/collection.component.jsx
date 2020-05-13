8a771330b0b903c3154db677c8f4c8166efb9637
unknown <aindriu80@gmail.com>
2020-05-13 14:55:29 +0100

    useEffect as ComponentWillUnmount()

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import { connect } from 'react-redux';
 
 import CollectionItem from '../../components/collection-item/collection-item.component';
 
 import { selectCollection } from '../../redux/shop/shop.selectors';
+
+import { firestore } from '../../firebase/firebase.utils';
 
 import {
   CollectionPageContainer,
@@ -13,6 +15,17 @@ import {
 } from './collection.styles';
 
 const CollectionPage = ({ collection }) => {
+  useEffect(() => {
+    console.log('i am subscribing');
+    const unsubscribeFromCollections = firestore
+      .collection('collections')
+      .onSnapshot(async snapshot => console.log(snapshot));
+    return () => {
+      console.log('I am unsubscribing');
+      unsubscribeFromCollections();
+    };
+  }, []);
+
   const { title, items } = collection;
   return (
     <CollectionPageContainer>
